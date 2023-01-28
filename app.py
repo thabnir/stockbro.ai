7bd5b7a8a247f483f0b7ec57b6842851eda0f4ec
Thabnir <thabtheman@gmail.com>
2023-01-28 11:47:56 -0500

    changed stuff ? ?! ?! ?!?!

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-from bokeh.embed import components
-from flask import Flask, render_template, request
-import pytrends
-from pytrends.request import TrendReq
-import matplotlib.pyplot as plt
-from bokeh.plotting import figure, show
-from bokeh.resources import CDN
 from bokeh.embed import file_html
-from io import BytesIO
-import base64
-import urllib
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from flask import Flask, render_template, request
+from pytrends.request import TrendReq
 
 app = Flask(__name__)
 
@@ -18,35 +12,32 @@ def index():
     return render_template('index.html')
 
 
-# Google trends' oldest date is 2004-01-01
-
 @app.route('/graph', methods=['POST'])
 def graph():
     # Get the user's input
     word = request.form['word']
-    # timeframe = request.form['timeframe']
     timeframe = request.form['timeframe-start'] + ' ' + request.form['timeframe-end']
-
-    # Authenticate and connect to the API
-    pytrends = TrendReq()
-
-    # Get data from the API
-    pytrends.build_payload(kw_list=[word], timeframe=timeframe)
-    data = pytrends.interest_over_time()
-
-    # Create new figure
-    # plt.figure()
+    data = get_trend_data(word, timeframe)
 
     # Create a plot (bokeh)
     bokeh_plot = figure(title=f'Popularity of "{word}" over time',
                         x_axis_label='Time',
                         x_axis_type='datetime',
                         y_axis_label='Popularity')
-    bokeh_plot.xaxis[0].ticker.desired_num_ticks = 7
     bokeh_plot.line(data.index, data[word], line_width=2)
-
+    bokeh_plot.xaxis[0].ticker.desired_num_ticks = 10
     html = file_html(bokeh_plot, CDN, "my plot")
-    return html
+    # embed html in the template (graph.html)
+    return render_template('graph.html', plot_div=html)
+
+
+def get_trend_data(word, timeframe, geo=''):
+    # Authenticate and connect to the API
+    pytr = TrendReq()
+    # Get data from the API
+    pytr.build_payload(kw_list=[word], timeframe=timeframe, geo=geo)
+    data = pytr.interest_over_time()
+    return data
 
 
 if __name__ == '__main__':
