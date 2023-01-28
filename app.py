c58c937492fa76ef93611d06e12a64d89c01b5d3
Thabnir <thabtheman@gmail.com>
2023-01-28 14:37:26 -0500

    progression

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from bokeh.embed import file_html
+from bokeh.models import Range1d, LinearAxis
 from bokeh.plotting import figure
 from bokeh.resources import CDN
-from bokeh.models import ColumnDataSource
 from flask import Flask, render_template, request
 from pytrends.request import TrendReq
 import yfinance as yf
+import logging
 
 app = Flask(__name__)
 
@@ -18,45 +19,47 @@ def index():
 def graph():
     # Get the user's input
     word = request.form['word']
-    stock = request.form['stock-ticker']
+    user_stock = request.form['stock-ticker']
     start = request.form['timeframe-start']
     end = request.form['timeframe-end']
+    html = graph_data(word, user_stock, start, end)
+    # embeds html in the template (graph.html)
+    return render_template('index.html', plot_div=html)
 
+
+def graph_data(word, user_stock, start, end):
     trend_data = get_word_data(word, start + ' ' + end)
-    stock_data = get_stock_data('AAPL', start, end)
+    stock_data = get_stock_data(user_stock, start, end)
 
     # Create a plot (bokeh)
-    bokeh_plot = figure(title=f'Popularity of "{word}" over time',
+    bokeh_plot = figure(title=f'Popularity of the word "{word}" and {user_stock} stock price over time',
                         x_axis_label='Date',
                         x_axis_type='datetime',
                         y_axis_label='Popularity',
                         )
-    bokeh_plot.line(trend_data.index, trend_data[word], line_width=2)
-    bokeh_plot.line(stock_data.index, stock_data['Close'], line_width=2, color='red')
+    # Plot word popularity
+    bokeh_plot.line(trend_data.index, trend_data[word], line_width=2, legend_label=word)
+    bokeh_plot.y_range = Range1d(trend_data[word].min() - 1, trend_data[word].max() + 1)
+
+    stock_range = "second y" + "_range"
+    bokeh_plot.extra_y_ranges = {stock_range: Range1d(stock_data['Close'].min() - 1, stock_data['Close'].max() + 1)}
+
+    # Plot stock price
+    bokeh_plot.line(stock_data.index,
+                    stock_data['Close'],
+                    line_width=2, color='red',
+                    legend_label=user_stock,
+                    y_range_name=stock_range
+                    )
+
+    bokeh_plot.add_layout(LinearAxis(y_range_name=stock_range), "right")
+
     bokeh_plot.xaxis[0].ticker.desired_num_ticks = 10
     bokeh_plot.legend.location = 'top_left'
     bokeh_plot.legend.click_policy = 'hide'
+
     html = file_html(bokeh_plot, CDN, "my plot")
-
-    # embed html in the template (graph.html)
-
-    # tech demo part
-    stocks = get_stocks_data('AAPL MSFT GOOG', start, end)
-
-    stocks_graph = figure(title="Tech Stocks", x_axis_label='Date', y_axis_label='Percentage Growth',
-                          x_axis_type='datetime')
-
-    for ticker in stocks['Close'].columns:
-        stocks_graph.line(stocks.index, stocks['Close', ticker], line_width=2, legend_label=ticker)
-
-    stocks_graph.legend.location = 'top_left'
-    stocks_graph.legend.click_policy = 'hide'
-
-    techdemo_html = file_html(stocks_graph, CDN, "tech demo plot")
-
-    # end of tech demo part
-
-    return render_template('graph.html', plot_div=html, techdemo_div=techdemo_html)
+    return html
 
 
 def get_word_data(word, timeframe, geo=''):
@@ -84,7 +87,7 @@ def get_stock_data(ticker_name, start, end):
 def get_stocks_data(ticker_names, start, end):
     app.logger.info(f'Getting data for {ticker_names} from {start} to {end}...')
     # this logging thing doesn't work for some reason, fix
-    tickers = yf.Tickers(ticker_names) # ex: 'AAPL MSFT GOOG'
+    tickers = yf.Tickers(ticker_names)  # ex: 'AAPL MSFT GOOG'
     stocks = tickers.history(start=start, end=end)
     return stocks
 
