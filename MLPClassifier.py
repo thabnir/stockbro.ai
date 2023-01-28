ed01194a6ff261b432e5a1d3f33f1ce666b0ab80
Thabnir <thabtheman@gmail.com>
2023-01-28 18:04:18 -0500

    good lord it (correlation) works!

--- MLPClassifier.py
+++ MLPClassifier.py
@@ -6,34 +6,36 @@ from sklearn.model_selection import train_test_split
 trend = pd.read_csv()
 stock = pd.read_csv()
 
-#Combine Google Trends and Stock Prices in one CSV file ("stock")
+# Combine Google Trends and Stock Prices in one CSV file ("stock")
 trends = []
 for trend in trend["NAME COLUMN"]:
-  trends.append(trend)
+    trends.append(trend)
 
 stock["Trend"] = trends
 
 data = stock
 print(data.head())
 
-#Clean up and preprocess the data
-def preprocess(data):
-    X = data.iloc[:]
-    y = data.iloc[:]  
 
-    return X, y
+# Clean up and preprocess the data
+def preprocess(data):
+    x = data.iloc[:]
+    y = data.iloc[:]
+
+    return x, y
+
 
 all_X, all_y = preprocess(data)
 
 X_train, X_test, y_train, y_test = train_test_split(all_X, all_y)
 print(X_train.shape)
 
-#MLP Classifier
+# MLP Classifier
 
 clf = MLPClassifier(max_iter=200)
 clf.fit(X_train, y_train)
 
 print(clf.score(X_test, y_test))
 
-#Predictions on new data
-clf.predict(X_test[15:25]) #some range
+# Predictions on new data
+clf.predict(X_test[15:25])  # some range
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import sys
 
+import pandas as pd
 from bokeh.embed import file_html
 from bokeh.models import Range1d, LinearAxis
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from flask import Flask, render_template, request
+from pandas._testing import iloc
 from pytrends.request import TrendReq
 import yfinance as yf
 import csv
@@ -18,37 +20,44 @@ with open("tickers.csv", "r") as csvfile:
     for row in reader:
         ticker_list.append(row[1])  # Add the ticker to the list
 
+
 @app.route('/')
 def index():
-    print('Hello world!', file=sys.stderr)
+    # print('Hello world!', file=sys.stderr)
     return render_template('index.html', ticker_list=ticker_list)
 
 
 @app.route('/graph', methods=['POST'])
 def graph():
     print('Trying to graph', file=sys.stderr)
-    for key in request.form:
-        print(f'{key}: {request.form[key]}', file=sys.stderr)
+    # for key in request.form:
+    #     print(f'{key}: {request.form[key]}', file=sys.stderr)
     # Get the user's input
     word = request.form['word']
     ticker = request.form['stock-ticker']
     start = request.form['timeframe-start']
     end = request.form['timeframe-end']
-    html = graph_data(word, ticker, start, end)
+
+    # Get the data
+    trend_data = get_trend_data(word, start + ' ' + end)
+    stock_data = get_stock_data(ticker, start, end)
+
+    html = graph_data(word, ticker, trend_data, stock_data)
+
+    correlation = get_correlation(trend_data[word], stock_data['Close'])
+    print(f'Correlation: {correlation}', file=sys.stderr)
     # embeds html in the template (graph.html)
-    return render_template('index.html', plot_div=html)
+    return render_template('index.html', plot_div=html, correlation=correlation)
 
 
-def graph_data(word, user_stock, start, end):
-    trend_data = get_word_data(word, start + ' ' + end)
-    stock_data = get_stock_data(user_stock, start, end)
-
+def graph_data(word, ticker, trend_data, stock_data):
+    print(f'Graphing data for {word} and {ticker}...', file=sys.stderr)
     # Create a plot (bokeh)
-    bokeh_plot = figure(title=f'Popularity of the word "{word}" and {user_stock} stock price over time',
+    bokeh_plot = figure(title=f'Popularity of the word "{word}" and {ticker} stock price over time',
                         x_axis_label='Date',
                         x_axis_type='datetime',
-                        y_axis_label='Popularity',
-                        )
+                        y_axis_label='Popularity')
+
     # Plot word popularity
     bokeh_plot.line(trend_data.index, trend_data[word], line_width=2, legend_label=word)
     bokeh_plot.y_range = Range1d(trend_data[word].min() - 1, trend_data[word].max() + 1)
@@ -60,9 +69,8 @@ def graph_data(word, user_stock, start, end):
     bokeh_plot.line(stock_data.index,
                     stock_data['Close'],
                     line_width=2, color='red',
-                    legend_label=user_stock,
-                    y_range_name=stock_range
-                    )
+                    legend_label=ticker,
+                    y_range_name=stock_range)
 
     bokeh_plot.add_layout(LinearAxis(y_range_name=stock_range), "right")
 
@@ -74,10 +82,8 @@ def graph_data(word, user_stock, start, end):
     return html
 
 
-def get_word_data(word, timeframe, geo=''):
-    app.logger.info(f'Getting data for {word} during {timeframe} at {geo}...')
-    # this logging thing doesn't work for some reason, fix
-
+def get_trend_data(word, timeframe, geo=''):
+    print(f'Getting data for {word} during {timeframe} at {geo}...', file=sys.stderr)
     # Authenticate and connect to the API
     pytr = TrendReq()
     # Get data from the API
@@ -87,18 +93,26 @@ def get_word_data(word, timeframe, geo=''):
 
 
 def get_stock_data(ticker_name, start, end):
-    app.logger.info(f'Getting data for {ticker_name} from {start} to {end}...')
-    # this logging thing doesn't work for some reason, fix
-
+    print(f'Getting data for {ticker_name} from {start} to {end}...', file=sys.stderr)
     ticker = yf.Ticker(ticker_name)
     # Get data from the API
     data = ticker.history(start=start, end=end)
     return data
 
 
+def get_correlation(word_data, stock_close_data):
+    print(f'Getting correlation between\n{word_data}\nand\n{stock_close_data}\n...', file=sys.stderr)
+    # convert stock_data from datetime64[ns, America/New_York] to datetime64[ns]
+    stock_close_data.index = stock_close_data.index.tz_localize(None)
+    net_data = pd.merge(word_data, stock_close_data, left_on='date', right_on='Date')
+    # print("Printing net data", file=sys.stderr)
+    # print(net_data, file=sys.stderr)
+    correlation = net_data[word_data.name].corr(net_data['Close'])
+    return correlation
+
+
 def get_stocks_data(ticker_names, start, end):
-    app.logger.info(f'Getting data for {ticker_names} from {start} to {end}...')
-    # this logging thing doesn't work for some reason, fix
+    print(f'Getting data for {ticker_names} from {start} to {end}...', file=sys.stderr)
     tickers = yf.Tickers(ticker_names)  # ex: 'AAPL MSFT GOOG'
     stocks = tickers.history(start=start, end=end)
     return stocks
