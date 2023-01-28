7009c33f4216f33091cfa31df03074b0ea38c298
clarariachi <122418329+clarariachi@users.noreply.github.com>
2023-01-28 18:40:27 -0500

    Update cohere.py

--- cohere.py
+++ cohere.py
@@ -1,67 +1,11 @@
 import cohere
 co = cohere.Client("<<TaTahdTgwZi7MIAPmTqwQQkhOJXxyX9ANEsvcuzR>>")
 
-prompt = f"""  
-This program generates a sassy bot comment given a (stock and a google search word) correlation coefficient
-
-Word: Bananas
-Stock: Microsoft  
-Comment: You can't be ugly AND not know the difference between causation and correlation, pick a struggle 
---  
-Word: Covid-19 
-Stock: Amazon
-Comment: Based on my calculations, you should invest all your money into Amazon as Covid-19's popularity grows. 
---  
-Word: Hippopotamus 
-Stock: Tesla
-Comment: 
-
---  
-Word: Education  
-Stock: Apple  
-Comment: 
-
---  
-Word: Canada
-Stock: Instacart
-Comment: 
-
---
-Word: 
-Stock:
-Comment: 
-
---
-Word: 
-Stock:
-Comment: 
-
---
-Word: 
-Stock:
-Comment: 
-
---
-Word: 
-Stock:
-Comment: 
-
---
-Word: 
-Stock:
-Comment: 
-
---
-Word: 
-Stock:
-Comment: 
-
-"""
-response = co.generate(
-    model='xlarge',
-    prompt = prompt,
-    max_tokens=50,
-    temperature=0.4,
+response = co.generate(  
+    model='xlarge',  
+    prompt = "Given a correlation coefficient between a google search's popularity and a stock's prices, this program generates a sassy bot comment.\n\nCorrelation Coefficient: -1\nComment:You can't be ugly AND not know the difference between causation and correlation, pick a struggle.\n--\nCorrelation Coefficient: 1\nComment: Based on my calculations, you should invest all your money into Amazon as Covid-19's popularity grows. An AI told you to do it, so it must be true, right?\n--Correlation Coefficient: -0.6\nComment: Bro just give up already; getting rich via the stock market is no way to get b*tches\n--\nCorrelation Coefficient: 0.5\nComment: If shes talking to you once a day im sorry bro thats not flirting that standup\n--Correlation Coefficient: -0.1\nComment: --\nPost: Going to unmute at the end of the Zoom meeting to say bye and realizing you were actually unmuted the whole call\nComment:",
+    max_tokens=50,  
+    temperature=0.4,  
     stop_sequences=["--"])
 
-comment = response.generations[0].text
+print('Insight: {}'.format(response.generations[0].text))
