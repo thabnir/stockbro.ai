6f5e43a27531fdff61e3d7326b0e329634e32d91
Thabnir <thabtheman@gmail.com>
2023-01-28 14:37:47 -0500

    Changes by Reyna

--- Cohere.py
+++ Cohere.py
@@ -1,11 +1,37 @@
 import cohere
-co = cohere.Client('3Mbg1ik6CxQKc2zS3iJMMzYB72ySlG8rFPTVr4Zj')
+co = cohere.Client('TaTahdTgwZi7MIAPmTqwQQkhOJXxyX9ANEsvcuzR')
 
-# generate a prediction for a prompt
-prediction = co.generate(
-    model='large',
-    prompt='co:here',
-    max_tokens=100)
+prompt = f"""  
+This program generates a sassy bot comment given a stock and a google search word
 
-# print the predicted text
-print('prediction: {}'.format(prediction.generations[0].text))
+Word: Bananas
+Stock: Microsoft  
+Comment: You can't be ugly AND not know the difference between causation and correlation, pick a struggle 
+--  
+Word: Covid-19 
+Stock: Amazon  
+Comment: Based on my calculations, you should invest all your money into Amazon as Covid-19's popularity grows. An AI told you to do it, so it must be true, right?
+--  
+Word: Hippopotamus attack
+Stock: Tesla
+Comment: 
+
+--  
+Word: Education  
+Stock: Apple  
+Comment: 
+
+--  
+Word: 
+Stock:
+Comment: 
+"""
+
+response = co.generate(
+    model='xlarge',
+    prompt = prompt,
+    max_tokens=50,
+    temperature=0.4,
+    stop_sequences=["--"])
+
+comment = response.generations[0].text
